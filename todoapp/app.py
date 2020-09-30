a455478416b59d6882ad34f76721fd7f4e1f7db5
btsixer <60943928+btsixer@users.noreply.github.com>
2020-09-30 14:27:16 -0500

    Add files via upload

---
+++ todoapp/app.py
@@ -0,0 +1,33 @@
+# Import Flask and SQLAlchemy to allow Python access to script to the database with Object Relational Mapping
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Set the flask instance to an app variable for later use
+app = Flask(__name__)
+# Defind the SQLAlchemy database connection string syntax postgresql://username:password@URL:port/databasename
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/todoapp'
+# Flask debug mode for live reloading in development mode
+app.run(debug=True)
+
+# Set a db instance to link the SQLAlchemy library to interact with the database
+db = SQLAlchemy(app)
+
+# Set a todo class to inherit from db.Model, with attributes mapped to the tablename and columns
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+# Detect models and create tables for them (if they don't exist already)
+db.create_all()
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=Todo.query.all()
+    )
+
+if __name__ == '__main__': 
+ app.run()
